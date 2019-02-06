b624a0067bb58e4083179fbba8ec6b702313c79e
luc.sh <me@luc.sh>
2019-02-06 18:09:18 -0300

    cambio ruta de persona

--- src/constants/BaseConfig.js
+++ src/constants/BaseConfig.js
@@ -5,8 +5,9 @@ export const SERVER_API = 'http://127.0.0.1:3009';
 export const LOGIN_PATH = '/';
 export const LOGOUT_PATH = '/logout';
 export const VERIFICATION_PATH = '/verify/:email/:verificationToken';
+//                                /verify/lucasdp@live.com/3ccf3196-de28-4e23-814c-59a50df7ef66
 
 // Apps spec
 export const DASHBOARD_PATH = '/escritorio';
 export const CUENTA_PATH = '/cuenta';
-export const PERSONA_PATH = '/persona';
+export const PERSONA_PATH = '/persona/:idUser';
