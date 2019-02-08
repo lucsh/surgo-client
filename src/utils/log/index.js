7c1afcfcb38a3a99b770ebb571d511ec896ec33e
luc.sh <me@luc.sh>
2019-02-08 16:37:15 -0300

    console logs

--- src/utils/log/index.js
+++ src/utils/log/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 const w = (msg) => console.warn(`%c${msg}`, 'background: #2E2E2E; color: #F2F5A9');
 const e = (msg) => console.error(`%c${msg}`, 'background: #FA5858; color: #F2F2F2');
-const i = (msg) => console.info(`%c${msg}`, 'background: #8181F7; color: #F2F2F2');
+const i = (msg) => console.info(`%c${msg}`, 'background: #8181F7; color: #F2F2F2; line-height:210%');
 const l = (msg, warp = '', sender) => {
   console.info(`%c /** START ${warp}`, 'background: #181817; color: #F2F2F2');
   sender &&
