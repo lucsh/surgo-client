b566481842b4879a0cbe4d9b17bc2f618f165c99
luc.sh <me@luc.sh>
2019-02-26 19:53:17 -0300

    WiP -_-

--- src/components/hitos/comentario.js
+++ src/components/hitos/comentario.js
@@ -7,8 +7,6 @@ import theme from '../../utils/theme/index';
 import { Image, More } from 'grommet-icons';
 import { READ_TRABAJOS } from '../../pages/cuenta/constants';
 
-require('moment/locale/es');
-
 class Comentario extends Component {
   render() {
     const BRAND_COLOR = theme.global.colors['brand'];
@@ -30,37 +28,41 @@ class Comentario extends Component {
       <ResponsiveContext.Consumer>
         {(size) => (
           <Box
+            border={'top'}
             alignContent="start"
             justify="start"
             align="start"
             direction={'row'}
-            pad="medium"
-            margin={{ top: '15px' }}
+            pad="none"
             width="large"
           >
             <Box
               alignContent="start"
               justify="start"
               align="start"
-              direction={'column'}
+              direction={'row'}
               pad="medium"
-              margin={{ top: '15px' }}
+              // margin={{ top: '15px' }}
               width="large"
             >
-              <Box align="start" gap="small">
+              <Box align="start" pad="small" gap="medium" direction={'column'}>
                 <img alt="Foto de perfil" className={'avatar-hito'} src={comentario.thumb} />
               </Box>
-              <Box align="start" gap="small">
-                <Text size={'small'} weight={'bold'} pad={'small'}>
-                  {comentario.createdBy} - {createdAt}
-                </Text>
-                <Text size={'small'} weight={'bold'} pad={'small'}>
-                  {reactStringReplace(comentario.comentario, /(#\S+\b)/gi, (match, i) => (
-                    <span key={i} style={{ color: BRAND_COLOR }}>
-                      {match}
-                    </span>
-                  ))}
-                </Text>
+              <Box align="start" pad="small" gap="small" direction={'column'}>
+                <Box align="start" pad="none" gap="none" direction={'row'}>
+                  <Text size={'13px'} color="brand">
+                    {comentario.createdBy} · {createdAt}
+                  </Text>
+                </Box>
+                <Box>
+                  <Text size={'13px'}>
+                    {reactStringReplace(comentario.comentario, /(#\S+\b)/gi, (match, i) => (
+                      <span key={i} style={{ color: BRAND_COLOR }}>
+                        {match}
+                      </span>
+                    ))}
+                  </Text>
+                </Box>
               </Box>
             </Box>
           </Box>
--- src/components/hitos/hito.js
+++ src/components/hitos/hito.js
@@ -1,51 +1,79 @@
 import React, { Component, Fragment } from 'react';
 import { Box } from 'grommet/es6/components/Box';
-import { Menu, ResponsiveContext, Text } from 'grommet/es6';
+import { Button, Form, FormField, Keyboard, Menu, ResponsiveContext, Text } from 'grommet/es6';
 import moment from 'moment';
 import theme from '../../utils/theme/index';
 import { More } from 'grommet-icons';
-import { READ_TRABAJOS } from '../../pages/cuenta/constants';
 import { Mutation } from 'react-apollo';
-import { DELETE_COMENTARIO } from '../../pages/persona/constants';
+import { DELETE_COMENTARIO, CREATE_COMENTARIO, READ_HITOS } from '../../pages/persona/constants';
 import Comentario from './comentario';
+import TextArea from './textAreaComentario';
+import SmallButton from '../smallButton';
+import ErrorComponent from '../error';
+import { FormClose } from 'grommet-icons';
+
+require('moment/locale/es');
 
 class Hito extends Component {
+  state = { showCommentForm: false };
+
   render() {
     const BRAND_COLOR = theme.global.colors['brand'];
 
     const eliminar = (value, mutation) => {
       mutation({
         variables: { id: value.id, idUser: value.idUser },
-        refetchQueries: [{ query: READ_TRABAJOS, variables: { idUser: value.idUser } }],
+        refetchQueries: [{ query: READ_HITOS, variables: { idUser: value.idUser } }],
       });
     };
 
     const eliminarComentario = (value, mutation) => {
       mutation({
         variables: { id: value.id, idUser: value.idUser },
-        refetchQueries: [{ query: READ_TRABAJOS, variables: { idUser: value.idUser } }],
+        refetchQueries: [{ query: READ_HITOS, variables: { idUser: value.idUser } }],
+      });
+    };
+
+    const saveComentario = (value, createCommet, idUser) => {
+      const data = {
+        comentario: value.estado,
+        estado: value.estado,
+      };
+      createCommet({
+        variables: { data, idUser },
+        refetchQueries: [{ query: READ_HITOS, variables: { idUser } }],
       });
     };
 
     const { hito, mutation } = this.props;
     const createdAt = moment(hito.createdAt).fromNow();
 
+    const { showCommentForm } = this.state;
     return (
       <ResponsiveContext.Consumer>
         {(size) => (
-          <Fragment>
+          <Box
+            alignContent="start"
+            justify="start"
+            align="start"
+            direction={'column'}
+            // pad="medium"
+            margin={{ top: '15px' }}
+            width="large"
+            elevation="xsmall"
+          >
             <Box
               alignContent="start"
               justify="start"
               align="start"
               direction={'column'}
               pad="medium"
-              margin={{ top: '15px' }}
+              // margin={{ top: '15px' }}
               width="large"
-              elevation="small"
+              // elevation="xsmall"
             >
               <Box
-                align="start"
+                align="center"
                 justify="between"
                 direction={size === 'xsmall' ? 'column' : 'row'}
                 width="large"
@@ -53,29 +81,16 @@ class Hito extends Component {
                 pad={'none'}
               >
                 <Box direction="row">
-                  <Text
-                    size={'xsmall'}
-                    weight={'normal'}
-                    margin={{ right: '5px', top: '2px' }}
-                    color={BRAND_COLOR}
-                  >
+                  <Text size={'xsmall'} weight={'normal'} margin={{ right: '5px' }} color={'brand'}>
                     &#11044;{'    '}
                   </Text>
-                  <Text size={'small'} weight={'bold'} pad={'small'}>
-                    {hito.estado}
+                  <Text size={'small'} pad={'small'}>
+                    Fue <span style={{ color: BRAND_COLOR }}>{hito.estado}</span> por{' '}
+                    <span style={{ color: BRAND_COLOR }}>{hito.createdBy}</span>
+                    <span style={{ color: '#7C8284' }}> · {createdAt}</span>
                   </Text>
                 </Box>
-                <Box
-                  align="start"
-                  direction={size === 'xsmall' ? 'column' : 'row'}
-                  gap={'none'}
-                  pad={'none'}
-                  justify={size === 'xsmall' ? 'start' : 'end'}
-                  width={size === 'xsmall' ? 'small' : 'medium'}
-                >
-                  <Text size={'xsmall'} weight={'bold'} color={'#7C8284'}>
-                    {hito.createdby} {createdAt}
-                  </Text>
+                <Box align="end" justify="end">
                   <Menu
                     alignSelf={'end'}
                     size="small"
@@ -92,19 +107,108 @@ class Hito extends Component {
                 </Box>
               </Box>
             </Box>
-              {hito.comentarios.map((comentario) => (
-                <Mutation key={comentario.id} mutation={DELETE_COMENTARIO}>
-                  {(deleteHito, { loading, error }) => (
-                    <Comentario
-                      comentario={comentario}
-                      loading={loading}
-                      error={error}
-                      mutation={eliminarComentario}
-                    />
+            <Mutation mutation={CREATE_COMENTARIO}>
+              {(createComment, { loading, error }) => (
+                <Box
+                  align="center"
+                  justify="center"
+                  direction={'row-responsive'}
+                  background={'#f5f8fa'}
+                  width={'large'}
+                  pad={{ vertical: 'xsmall' }}
+                >
+                  {!showCommentForm && (
+                    <Box
+                      align="center"
+                      justify={'center'}
+                      direction={'row-responsive'}
+                      width={'medium'}
+                      animation={'fadeIn'}
+                      background={'white'}
+                      round={'xsmall'}
+                    >
+                      <Button plain onClick={() => this.setState({ showCommentForm: true })}  style={{ color: '#7C8284', fontSize: '10px' }}>
+                        Agregar nuevo comentario o #etiquetas
+                      </Button>
+                    </Box>
                   )}
-                </Mutation>
-              ))}
-          </Fragment>
+                  {showCommentForm && (
+                    <Box align="start" alignSelf="start">
+                      <Box
+                        align="center"
+                        justify="center"
+                        alignSelf="end"
+                        pad={'small'}
+                      >
+                        <Button
+                          onClick={() => this.setState({ showCommentForm: false })}
+                          type="button"
+                          plain
+                          label="&times;"
+                          style={{ color: '#F04953', fontSize: '18px' }}
+                        />
+                      </Box>
+                      <Box
+                        align="center"
+                        justify={'center'}
+                        direction={'row-responsive'}
+                        width={'large'}
+                        animation={'fadeIn'}
+                      >
+                        <Form
+                          onSubmit={({ value }) =>
+                            saveComentario(value, createComment, this.props.user.id)
+                          }
+                          value={{
+                            comentario: '',
+                          }}
+                        >
+                          <Keyboard
+                            onEsc={() => {
+                              this.setState({ showCommentForm: false });
+                            }}
+                          >
+                            <Box align="start" direction={'row-responsive'} gap={'none'}>
+                              <FormField
+                                label=""
+                                name="comentario"
+                                required
+                                width={'large'}
+                                placeholder="Agregar nuevo comentario o #etiquetas"
+                                rows="1"
+                                component={TextArea}
+                              />
+                            </Box>
+                          </Keyboard>
+                          <Box align="end" alignSelf="end" pad={{ horizontal: 'small' }}>
+                            <SmallButton
+                              type="button"
+                              label="+ Agregar"
+                              onClick={this.handleAgregar}
+                              color="brand"
+                            />
+                          </Box>
+                          {error && <ErrorComponent error={error} />}
+                        </Form>
+                      </Box>
+                    </Box>
+                  )}
+                </Box>
+              )}
+            </Mutation>
+            {hito.comentarios.map((comentario) => (
+              <Mutation key={comentario.id} mutation={DELETE_COMENTARIO}>
+                {(deleteHito, { loading, error }) => (
+                  <Comentario
+                    comentario={comentario}
+                    loading={loading}
+                    error={error}
+                    mutation={eliminarComentario}
+                  />
+                )}
+              </Mutation>
+            ))}
+          </Box>
         )}
       </ResponsiveContext.Consumer>
     );
